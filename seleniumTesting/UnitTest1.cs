00a1f328a73a30f1b52b12f73b8514e6f277eed1
kenmk9 <130543191+kenmk9@users.noreply.github.com>
2024-09-09 10:06:50 +0000

    added selenium tests

---
+++ seleniumTesting/UnitTest1.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;			
+using OpenQA.Selenium.Chrome;	
+
+namespace SeleniumBingTests
+{
+  /// <summary>
+  /// Summary description for MySeleniumTests
+  /// </summary>
+  [TestClass]
+  public class MySeleniumTests
+  {
+    private TestContext testContextInstance;
+    private IWebDriver driver;
+    private string appURL;
+
+
+    [TestMethod]
+    [TestCategory("Chrome")]
+    public void TheBingSearchTest()
+    {
+      Assert.IsTrue(driver.Title.Contains("Google"), "Verified title of the page");
+    }
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    [TestInitialize()]
+    public void SetupTest()
+    {
+      appURL = "http://www.google.com/";
+
+      string browser = "Chrome";
+      switch(browser)
+      {
+        case "Chrome":
+          driver = new ChromeDriver();
+          break;
+        default:
+          driver = new ChromeDriver();
+          break;
+      }
+
+    }
+
+    [TestCleanup()]
+    public void MyTestCleanup()
+    {
+      driver.Quit();
+    }
+  }
+}
